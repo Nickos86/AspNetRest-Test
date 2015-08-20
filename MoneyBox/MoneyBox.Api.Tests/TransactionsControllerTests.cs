d4343c6c7805e49bab29bcd23758e808d6b80eb5
Nick <nickwardesq@hotmail.com>
2015-08-20 11:28:14 +0100

    initial projects commit with first unit test

---
+++ MoneyBox/MoneyBox.Api.Tests/TransactionsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MoneyBox.Api.Tests
+{
+    [TestFixture]
+    public class TransactionsControllerTests
+    {
+        [Test]
+        public void Get_CallGet_ReturnsListOfTransactions()
+        {
+            var mockRepository = new Mock<ITransactionRepository>();
+            var transactionController = new TransactionsController(mockRepository.Object);
+
+            var transactions = transactionController.Get();
+
+            Assert.That(transactions.length, Is.GreaterThan(0));
+        }
+    }
+}
